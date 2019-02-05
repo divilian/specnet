2a8a4fae893cbd7f8239ae4b20adfb6fbac03785
Stephen <stephen@umw.edu>
2019-02-05 16:06:56 -0500

    Add outer loop and node selection.

--- sim.jl
+++ sim.jl
@@ -5,26 +5,53 @@ using LightGraphs
 using GraphPlot, Compose
 using Colors
 
+params = Dict(
+    "N" => 20,
+    "openness" => .1, 
+    "num_iter" => 100
+)
+
 println("Running SriMilG...")
 if length(ARGS) == 2
-    defaults = false
-    N = ARGS[1]
-    openness = ARGS[2]
+    using_defaults = false
+    params["N"] = ARGS[1]
+    params["openness"] = ARGS[2]
+    params["num_iter"] = ARGS[3]
 elseif length(ARGS) == 0
-    defaults = true
-    N = 20
-    openness = .1
+    using_defaults = true
 else
-    println("Usage: sim.jl N openness.")
+    println("Usage: sim.jl N openness num_iter.")
 end
 
-println("Using " * (defaults ? "defaults" : "") *
-    " (N=$(N), openness=$(openness))...")
+println("Using" * (using_defaults ? " defaults" : "") * ":")
+display(params)
+
+# There must be a better way to do this using eval():
+N = params["N"]
+openness = params["openness"]
+num_iter = params["num_iter"]
+
+
 
 colors = rand((colorant"lightblue",colorant"red"), N)
+wealths = rand(Float16, N)
 
 graph = LightGraphs.SimpleGraphs.erdos_renyi(N,.2)
 gplot(graph, 
     nodelabel=1:N,
     NODESIZE=.08,
     nodefillc=colors)
+
+for iter in 1:num_iter
+    println("Iteration $(iter)...")
+    node1 = rand(1:N)
+    if rand(Float16) < openness
+        # Choose from the graph at large.
+        node2 = rand(filter(x->x!=node1,1:N))
+        println("$(node1), and at-large $(node2)")
+    else
+        # Choose from a neighbor.
+        node2 = rand(neighbors(graph,node1))
+        println("$(node1), and neighbor $(node2)")
+    end
+end
