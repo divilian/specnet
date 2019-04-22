b25b0df6e1a4c161a8644b5d2a28e6b6f6ec4e6b
Stephen <stephen@umw.edu>
2019-04-22 10:49:59 -0400

    Fix indentation and a couple whitespace errors.

--- src/net/sim.jl
+++ src/net/sim.jl
@@ -95,33 +95,34 @@ function specnet(params)
     function wealth_eligible(agent)
            return wealths[agent] > proto_threshold
     end
-    # agent joins preexisting proto
-    function join_proto(agent1,proto)
-        push!(proto,agent1)
-    for agent in proto
-                if !has_edge(graph, AN[agent], AN[agent1])
-                    add_edge!(graph, AN[agent], AN[agent1])
-                end
-		    end
-		end
-		
-		#return the proto a given agent is in
-    function get_proto(agent1)
-      if(!in_proto(agent1))
-            return -1
-            else
-		for proto in protos 
-            if agent1 in proto
-                return proto
-            	 end
-		end
-	end
-    end
-    
+
     # Form a new proto between two agents.
     function form_proto(agent1, agent2)
         global protos
         push!(protos, Set{Char}([agent1,agent2]))
+    end
+
+    # agent joins preexisting proto
+    function join_proto(agent1,proto)
+        push!(proto,agent1)
+        for agent in proto
+            if !has_edge(graph, AN[agent], AN[agent1])
+                add_edge!(graph, AN[agent], AN[agent1])
+            end
+        end
+    end
+		
+    #return the proto a given agent is in
+    function get_proto(agent1)
+        if(!in_proto(agent1))
+            return -1
+        else
+            for proto in protos
+                if agent1 in proto
+                    return proto
+                end
+            end
+        end
     end
 
     function assert_no_dead_neighbors(graph, agent, dead)
@@ -224,13 +225,13 @@ function specnet(params)
                 add_edge!(graph, AN[agent1], AN[agent1])
             end
         end
-                                                                                                #allow agent to join proto 
+                                                                                                #allow agent to join proto
 	    if wealth_eligible(agent1) && wealth_eligible(agent2)
 	        if in_proto(agent1)&&!in_proto(agent2)
 	            current_proto=get_proto(agent1)
 	            join_proto(agent2,current_proto)
 	        end
-           
+
            if in_proto(agent2)&&!in_proto(agent1)
 	                current_proto=get_proto(agent2)
 	                join_proto(agent1,current_proto)
